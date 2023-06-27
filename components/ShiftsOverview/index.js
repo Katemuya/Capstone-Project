79f1e04c9e2e39d8428e0e6cebfeccf460831c03
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-06-27 14:59:21 +0200

    create, edit and refactore shifts Components

--- components/ShiftsOverview/index.js
+++ components/ShiftsOverview/index.js
@@ -1,43 +1,63 @@
 import styled from "styled-components";
-import ShiftDialog from "../Dialog";
 import Link from "next/link";
 import { useState } from "react";
+import ShiftItem from "../ShiftItem";
 
 const StyledShifts = styled.div`
   left: 30%;
 `;
-const EditButton = styled.button`
-  position: absolute;
-  right: 30%;
-`;
 
-export default function ShiftsOverview() {
+const initialShiftsInfo = [
+  {
+    id: 0,
+    shiftName: "Morning Shift 🌅",
+    start: "00:00",
+    end: "00:00",
+  },
+  {
+    id: 1,
+    shiftName: "Afternoon shift 🌞",
+    start: "00:00",
+    end: "00:00",
+  },
+
+  {
+    id: 3,
+    shiftName: "Night Shift 🌙",
+    start: "00:00",
+    end: "00:00",
+  },
+];
+export default function ShiftsOverview({ shiftFormData }) {
   //Define useState for both the edit and close buttons
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [shiftsInfo, setShiftsInfo] = useState(initialShiftsInfo);
   //Toggle the edit button
-  const handleToggleEdit = () => {
-    setIsDialogOpen(!isDialogOpen);
+
+  const updateNewShiftsInfo = (updatedShiftInfo) => {
+    console.log(updatedShiftInfo);
+    // console.log(shiftsInfo[0]);
+    setShiftsInfo(
+      shiftsInfo.map((shiftInfo) => {
+        return shiftInfo.id === updatedShiftInfo.id
+          ? updatedShiftInfo
+          : shiftInfo;
+      })
+    );
   };
-  //define the shifts under ShiftItem
-  const ShiftItem = ({ title, time }) => (
-    <div>
-      <EditButton onClick={handleToggleEdit}>Edit</EditButton>
-      {isDialogOpen && <ShiftDialog onClose={() => setIsDialogOpen(false)} />}
-      <div>
-        <h3>{title}</h3>
-        <p>{time}</p>
-      </div>
-    </div>
-  );
+
   return (
     <StyledShifts>
-      <ul>
-        <h1>My Shifts</h1>
-        <ShiftItem title="Morning Shift" time="(6:00- 14:00)" />
-        <ShiftItem title="Afternoon Shift" time="(14:00- 20:00)" />
-        <ShiftItem title="Night Shift" time="(20:00- 06:00)" />
-        <ShiftItem title="Other" time="(8:00- 4:00)" />
-      </ul>
+      {shiftsInfo.map((shiftInfo) => (
+        <div key={shiftInfo.id}>
+          <ShiftItem
+            shiftInfo={shiftInfo}
+            title={shiftInfo.shiftName}
+            time={`${shiftInfo.start} - ${shiftInfo.end}`}
+            updateNewShiftsInfo={updateNewShiftsInfo}
+          ></ShiftItem>
+        </div>
+      ))}
+
       <Link href="./"> Back to Calendar </Link>
     </StyledShifts>
   );
