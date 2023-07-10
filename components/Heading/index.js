c8cf244ce5672943a1c786523b8b5c85ebbd6675
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-07-10 15:14:00 +0200

    create Heading

--- components/Heading/index.js
+++ components/Heading/index.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
-const Heading = styled.h1`
+export const StyledHeading = styled.h1`
   text-align: center;
+  background-color: #f0ebe3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: #576f72;
 `;
-export default Heading;
+
+export default function Heading() {
+  return <StyledHeading>ShiftPro</StyledHeading>;
+}
