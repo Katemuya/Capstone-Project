8859a80cfd03a9da9199e7b30a3b9912d5296d67
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-07-10 15:29:12 +0200

    pass handle color chnaeg prop

--- components/Dialog/index.js
+++ components/Dialog/index.js
@@ -6,6 +6,7 @@ export default function ShiftDialog({
   onClose,
   updateNewShiftsInfo,
   deleteShift,
+  handleColorChange,
 }) {
   return (
     <div>
@@ -15,6 +16,7 @@ export default function ShiftDialog({
           updateNewShiftsInfo={updateNewShiftsInfo}
           deleteShift={deleteShift}
           closeDialog={onClose}
+          handleColorChange={handleColorChange}
         ></EditShift>
       </MainDialog>
     </div>
