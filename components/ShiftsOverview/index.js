137b762b4dccfbd6442ceee0649ee82aa38316f5
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-07-10 15:13:10 +0200

    create NavigationBar Component

--- components/ShiftsOverview/index.js
+++ components/ShiftsOverview/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Link from "next/link";
-
+import { StyledHeading } from "../Heading";
 import ShiftItem from "../ShiftItem";
 import NewShifts from "../NewShifts";
 import { useState } from "react";
@@ -9,19 +9,38 @@ import { Button } from "../EditShift";
 
 const StyledShifts = styled.div`
   left: 30%;
+  padding-top: 2em;
+  align-content: center;
 `;
-const CreateButton = styled(Button)`
-  background-color: #f2be22;
+
+const StyledContainer = styled.div`
+  background-color: #e5e3c9;
+  height: 90vh;
+  width: 100%;
+  overflow: scroll;
+`;
+const StyledCreateButton = styled(Button)`
+  background-color: #3e8e7e;
+  margin: 10px;
   &:hover {
     background-color: #ffc95f;
   }
 `;
+const StyledShiftsHeading = styled(StyledHeading)`
+  background-color: #e5e3c9;
+`;
 
 export default function ShiftsOverview({ shiftsInfo, setShiftsInfo }) {
+  const [isShiftDialogOpen, setIsShiftDialogOpen] = useState(false);
+  const [isNewShiftDialogOpen, setIsNewShiftDialogOpen] = useState(false);
+  const [selectedShiftId, setSelectedShiftId] = useState(null);
+
+  //function to handle adding a new shift
   const handleAddShift = (newShift) => {
     setShiftsInfo([...shiftsInfo, newShift]);
   };
 
+  //function to update new shifts
   const updateNewShiftsInfo = (updatedShiftInfo) => {
     setShiftsInfo(
       shiftsInfo.map((shiftInfo) => {
@@ -31,10 +50,6 @@ export default function ShiftsOverview({ shiftsInfo, setShiftsInfo }) {
       })
     );
   };
-
-  const [isShiftDialogOpen, setIsShiftDialogOpen] = useState(false);
-  const [isNewShiftDialogOpen, setIsNewShiftDialogOpen] = useState(false);
-  const [selectedShiftId, setSelectedShiftId] = useState(null);
 
   const deleteShift = (shiftId) => {
     const confirmed = window.confirm(
@@ -47,46 +62,46 @@ export default function ShiftsOverview({ shiftsInfo, setShiftsInfo }) {
     }
   };
   return (
-    <StyledShifts>
-      {shiftsInfo &&
-        shiftsInfo.map((shiftInfo) => (
-          <div key={shiftInfo.id}>
-            <ShiftItem
-              title={shiftInfo.shiftName}
-              time={`${shiftInfo.start} - ${shiftInfo.end}`}
-              onEdit={() => {
-                setIsShiftDialogOpen(true);
-                setSelectedShiftId(shiftInfo.id);
-              }}
-            ></ShiftItem>
-
-            {isShiftDialogOpen && selectedShiftId === shiftInfo.id && (
-              <ShiftDialog
-                shiftInfo={shiftInfo}
-                updateNewShiftsInfo={updateNewShiftsInfo}
-                deleteShift={deleteShift}
-                onClose={() => {
-                  setIsShiftDialogOpen(false);
-                  setSelectedShiftId(null);
+    <StyledContainer>
+      <StyledShiftsHeading>My Shifts</StyledShiftsHeading>
+      <StyledShifts>
+        {shiftsInfo &&
+          shiftsInfo.map((shiftInfo) => (
+            <div key={shiftInfo.id}>
+              <ShiftItem
+                title={shiftInfo.shiftName}
+                time={`${shiftInfo.start} - ${shiftInfo.end}`}
+                selectedColor={shiftInfo.color}
+                onEdit={() => {
+                  setIsShiftDialogOpen(true);
+                  setSelectedShiftId(shiftInfo.id);
                 }}
-              ></ShiftDialog>
-            )}
+              ></ShiftItem>
 
-            {isNewShiftDialogOpen && (
-              <NewShifts
-                handleAddShift={handleAddShift}
-                closeDialog={() => setIsNewShiftDialogOpen(false)}
-              ></NewShifts>
-            )}
-          </div>
-        ))}
-      <div>
-        <CreateButton onClick={() => setIsNewShiftDialogOpen(true)}>
-          Create Shift
-        </CreateButton>
-      </div>
+              {isShiftDialogOpen && selectedShiftId === shiftInfo.id && (
+                <ShiftDialog
+                  shiftInfo={shiftInfo}
+                  updateNewShiftsInfo={updateNewShiftsInfo}
+                  deleteShift={deleteShift}
+                  onClose={() => {
+                    setIsShiftDialogOpen(false);
+                    setSelectedShiftId(null);
+                  }}
+                ></ShiftDialog>
+              )}
 
-      <Link href="./"> Back to Calendar </Link>
-    </StyledShifts>
+              {isNewShiftDialogOpen && (
+                <NewShifts
+                  handleAddShift={handleAddShift}
+                  closeDialog={() => setIsNewShiftDialogOpen(false)}
+                ></NewShifts>
+              )}
+            </div>
+          ))}
+      </StyledShifts>
+      <StyledCreateButton onClick={() => setIsNewShiftDialogOpen(true)}>
+        Create Shift
+      </StyledCreateButton>
+    </StyledContainer>
   );
 }
